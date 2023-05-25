757fd1dc7d9ab082981e3b8269c28995b83aa5d1
Salatiel dos Reis Gomes <salatieldrg@gmail.com>
2023-05-25 20:47:35 -0300

    Endpoint de consultas de médicos

---
+++ src/main/java/med/voll/api/model/dto/DadosListagemMedico.java
@@ -0,0 +1,10 @@
+package med.voll.api.model.dto;
+
+import med.voll.api.enums.Especialidade;
+import med.voll.api.model.Medico;
+
+public record DadosListagemMedico(String nome, String email, String crm, Especialidade especialidade) {
+    public DadosListagemMedico(Medico medico){
+        this(medico.getNome(), medico.getEmail(), medico.getCrm(), medico.getEspecialidade());
+    }
+}
