cdf80b8dcd13e8653aec5ddb39a42834b3c64dab
Salatiel dos Reis Gomes <salatieldrg@gmail.com>
2023-05-25 21:26:46 -0300

    Parâmetros default da paginação/ordenação

--- src/main/java/med/voll/api/controller/MedicoController.java
+++ src/main/java/med/voll/api/controller/MedicoController.java
@@ -8,6 +8,8 @@ import med.voll.api.repository.MedicoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.*;
 
@@ -27,7 +29,8 @@ public class MedicoController {
     }
 
     @GetMapping
-    public Page<DadosListagemMedico> listar(Pageable paginacao){
+    public Page<DadosListagemMedico> listar(@PageableDefault(size=10, page=0, sort = {"nome"}, direction = Sort.Direction.DESC) Pageable paginacao){
+        //http://localhost:8080/medicos?size=1&page=2&sort=nome,desc parâmetros passados na url sobrescrevem o padrão
         return repository.findAll(paginacao).map(DadosListagemMedico::new);
     }
 }
