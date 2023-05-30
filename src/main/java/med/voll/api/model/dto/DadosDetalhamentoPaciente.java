f57a7b54a083c1c341cab62e15c090a8c8f2416a
Salatiel dos Reis Gomes <salatieldrg@gmail.com>
2023-05-30 19:22:36 -0300

    Cadastro dos endpoints dos pacientes

---
+++ src/main/java/med/voll/api/model/dto/DadosDetalhamentoPaciente.java
@@ -0,0 +1,11 @@
+package med.voll.api.model.dto;
+
+import med.voll.api.model.Endereco;
+import med.voll.api.model.Paciente;
+
+public record DadosDetalhamentoPaciente(Long id, String nome, String email, String cpf, String telefone, Endereco endereco) {
+
+    public DadosDetalhamentoPaciente(Paciente paciente) {
+        this(paciente.getId(), paciente.getNome(), paciente.getEmail(), paciente.getCpf(), paciente.getTelefone(), paciente.getEndereco());
+    }
+}
