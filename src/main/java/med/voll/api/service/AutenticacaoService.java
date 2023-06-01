292f2a2d72df5273ef889b458f6d16234b9cf3ec
Salatiel dos Reis Gomes <salatieldrg@gmail.com>
2023-05-31 21:31:12 -0300

    Implementação do processo de autenticação da API

---
+++ src/main/java/med/voll/api/service/AutenticacaoService.java
@@ -0,0 +1,20 @@
+package med.voll.api.service;
+
+import lombok.AllArgsConstructor;
+import med.voll.api.repository.UsuarioRepository;
+import org.springframework.security.core.userdetails.AuthenticationUserDetailsService;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+@AllArgsConstructor
+public class AutenticacaoService implements UserDetailsService {
+
+    private final UsuarioRepository usuarioRepository;
+    @Override
+    public UserDetails loadUserByUsername(String login) throws UsernameNotFoundException {
+        return usuarioRepository.findByLogin(login);
+    }
+}
