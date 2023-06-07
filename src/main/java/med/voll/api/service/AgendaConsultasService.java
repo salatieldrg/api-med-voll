7c35fa50a7d8c09a56e0ebb2f9c0bad16d6cdb37
Salatiel dos Reis Gomes <salatieldrg@gmail.com>
2023-06-07 19:37:11 -0300

    Validações de cadastro de consultas

--- src/main/java/med/voll/api/service/AgendaConsultasService.java
+++ src/main/java/med/voll/api/service/AgendaConsultasService.java
@@ -1,28 +1,55 @@
 package med.voll.api.service;
 
 import med.voll.api.domain.Consulta;
+import med.voll.api.domain.Medico;
+import med.voll.api.domain.ValidacaoException;
 import med.voll.api.domain.dto.DadosAgendamentoConsulta;
 import med.voll.api.repository.ConsultaRepository;
 import med.voll.api.repository.MedicoRepository;
 import med.voll.api.repository.PacienteRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class AgendaConsultasService {
 
+    @Autowired
     private ConsultaRepository consultaRepository;
+
+    @Autowired
     private MedicoRepository medicoRepository;
 
+    @Autowired
     private PacienteRepository pacienteRepository;
 
     public void agendar(DadosAgendamentoConsulta dados){
 
-        var medico = medicoRepository.findById(dados.idMedico()).get();
-        var paciente = pacienteRepository.findById(dados.idPaciente()).get();
+        if(!pacienteRepository.existsById(dados.idPaciente())){
+            throw new ValidacaoException("Não existe nenhum paciente com o ID informado");
+        }
+
+        if(dados.idMedico() != null && !medicoRepository.existsById(dados.idMedico())){
+            throw new ValidacaoException("Não existe nenhum médico com o ID informado");
+        }
+
+        var medico = escolherMedico(dados);
+        var paciente = pacienteRepository.getReferenceById(dados.idPaciente());
 
         var consulta = new Consulta(null, medico, paciente, dados.data());
         consultaRepository.save(consulta);
 
+    }
+
+    private Medico escolherMedico(DadosAgendamentoConsulta dados) {
+        if(dados.idMedico() != null){
+            return medicoRepository.getReferenceById(dados.idMedico());
+        }
+
+        if(dados.especialidade() == null){
+            throw new ValidacaoException("Especialidade é obrigatória quando não for informado médico.");
+        }
+
+        return medicoRepository.escolherMedicoAleatorioLivreNaData(dados.especialidade(), dados.data());
     }
 
 }
